fed20d213d9859c105ce3960bfd8d7c54732f9ad
soukainahikma <soukainahikma@gmail.com>
2024-04-29 12:22:25 +0100

    save modifications

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import pandas as pd
 from ft_linear_regression import LinearRegression
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score
 df = pd.read_csv('./data.csv')
 X = df[['km']].values
 y = df['price'].values
@@ -18,8 +19,18 @@ reg = LinearRegression()
 reg.fit(X_train_scaled, y_train)
 
 y_pred_line = reg.predict(X_test_scaled)
+
+reg1 = LinearRegression(n_iters=10000)
+reg1.fit(X_train_scaled, y_train)
+y_pred_line1 = reg1.predict(X_test_scaled)
+print(reg.weight, reg.bias)
+
+print(r2_score(y_test, y_pred_line))
+
 fig = plt.figure(figsize=(8, 6))
 m1 = plt.scatter(X_train, y_train, color="red", s=30)
 m2 = plt.scatter(X_test, y_test, color="blue", s=30)
 plt.plot(X_test, y_pred_line, color='black', linewidth=2, label='Prediction')
+plt.plot(X_test, y_pred_line1, color='green', linewidth=2, label='Prediction')
+
 plt.show()
